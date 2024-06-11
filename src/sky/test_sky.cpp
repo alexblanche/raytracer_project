8310e0182d6ded6588da5d0a96993232921f682b
Alexandre Blanché <blanchealexandre11@yahoo.fr>
2024-06-11 18:02:34 +0200

    Sky trigonometry calculations (not working yet)

--- src/sky/test_sky.cpp
+++ src/sky/test_sky.cpp
@@ -6,12 +6,18 @@
 #include <algorithm>
 // #include <thread>
 // #include <mutex>
-// #include <functional>
+#include <functional>
 #include <chrono>
 
-#include<unistd.h>
+// #include<unistd.h>
 
-/* Attempt at a real-time skydome */
+#include <cmath>
+
+#include "light/vector.hpp"
+
+#define PI 3.14159265359f
+
+/* Prototype: attempt at a real-time skydome, to be cleaned-up later */
 
 /* Returns the current time in milliseconds */
 uint64_t get_time() {
@@ -21,30 +27,34 @@ uint64_t get_time() {
 }
 
 struct mouse_pos {
-    int x;
-    int y;
+    const float width;
+    const float height;
+    float theta, phi;
 
-    void set(int mx, int my) {
-        x = mx;
-        y = my;
+    mouse_pos(int width, int height)
+        : width(width), height(height),
+          theta(0), phi(0.5) {}
+
+    // xr and yr are relative positions
+    void set(int xr, int yr) {
+        theta += xr / width;
+        phi += yr / height;
+
+        if (theta > 1) {
+            theta -= 1;
+        }
+        else if (theta < 0) {
+            theta += 1;
+        }
+        
+        if (phi > 1) {
+            phi = 1;
+        }
+        else if (phi < 0) {
+            phi = 0;
+        }
     }
 };
-
-void update_screen_cursor(rt::screen& scr, const rt::color& bg_col, const rt::color& cursor_col, mouse_pos& mouse) {
-    scr.clear();
-    scr.fill_rect(0, 0, scr.width()-1, scr.height()-1, bg_col);
-    //SDL_GetMouseState(&mouse.x, &mouse.y);
-    scr.draw_line(
-        mouse.x - 8, mouse.y,
-        mouse.x + 8, mouse.y,
-        cursor_col
-    );
-    scr.draw_line(
-        mouse.x, mouse.y - 8,
-        mouse.x, mouse.y + 8,
-        cursor_col
-    );
-}
 
 void update_screen(rt::screen& scr, std::vector<std::vector<rt::color>>& image, mouse_pos& /*mouse*/) {
     scr.clear();
@@ -56,6 +66,86 @@ void update_screen(rt::screen& scr, std::vector<std::vector<rt::color>>& image, 
         }
     }
 }
+
+// Index of the pixel in the original texture corresponding to the index i, j of the screen
+int index_source (const int i, const int j, const float& ratio_x, const int img_width) {
+
+    // Copying the image onto the screen
+    return 3 * (j * img_width + ((int) (ratio_x * i)));
+}
+
+
+/** Trigonometry calculations **/
+
+// Struct containing the X and Y axes of the screen in world space
+struct screen_axes {
+    rt::vector screen_x_axis;
+    rt::vector screen_y_axis;
+
+    screen_axes() {
+        screen_x_axis = rt::vector(1, 0, 0);
+        screen_y_axis = rt::vector(0, 1, 0);
+    }
+
+    void set(const float& theta, const float& phi) {
+        const float sinphi = sin(phi);
+        const float costheta = cos(theta);
+        const float sintheta = sin(theta);
+        screen_x_axis.x = sintheta;
+        screen_x_axis.z = costheta;
+        screen_y_axis.x = -sinphi * costheta;
+        screen_y_axis.y = costheta;
+        screen_y_axis.z = sinphi * sintheta;
+    }
+};
+
+// Returns the catersian coordinates of the center of the screen (x0, y0, z0)
+// The 4 edges of the screen plane are set at radius 1, so the center is
+// set at a distance cos(fov_x)
+rt::vector get_center(const float& fov_x, const float& theta, const float& phi) {
+    const float l = cos(fov_x * PI);
+    const float lcosphi = l * cos(phi);
+    return rt::vector(
+        lcosphi * cos(theta),
+        l * sin(phi),
+        lcosphi * (-sin(theta))
+    );
+}
+
+// Returns the world space cartesian coordinates of the pixel of coordinates (i, j) (in screen space)
+rt::vector get_cartesian(screen_axes& axes,
+    const float& fov_x, const float& fov_y, const int scr_width, const int scr_height,
+    const float& theta, const float& phi, const int i, const int j) {
+
+    const rt::vector center = get_center(fov_x, theta, phi);
+    axes.set(theta, phi);
+    const int i0 = scr_width / 2;
+    const int j0 = scr_height / 2;
+    return center
+        + axes.screen_x_axis * (i - i0) * (fov_x * PI / i0)  // atomic horizontal angle: fov_x * pi / (width / 2)
+        + axes.screen_y_axis * (j0 - j) * (fov_y * PI / j0); // atomic vertical angle: fov_y * pi / (height / 2)
+}
+
+// Struct for spherical coordinates
+struct spherical {
+    float theta;
+    float phi;
+
+    // Returns the world space spherical coordinates (theta, phi) of the point of cartesian coordinates (x, y, z)
+    // (assuming sqrt(x*x + y*y + z*z) < 1)
+    spherical(const rt::vector& u) {
+        if (u.x == 0) {
+            theta = PI / 2;
+        }
+        else {
+            theta = atan(u.z / u.x);
+        }
+
+        phi = asin(u.y);
+    }
+};
+
+
 
 int main(int, char**) {
 
@@ -74,24 +164,18 @@ int main(int, char**) {
         return EXIT_FAILURE;
     }
 
-    std::cout << "Hello" << std::endl;
-
     /* Screen dimensions */
     //int width = 1366, height = 768;
     int width = 1920, height = 1080;
 
     rt::screen scr(width, height);
 
-    std::cout << "Screen created" << std::endl;
+    SDL_ShowCursor(SDL_DISABLE);
 
     const unsigned int depth = 3;
 
     const unsigned int nbpix = depth * dims.value().width * dims.value().height;
     std::vector<char> pixels(nbpix);
-
-    // std::cout << "Pixel vector created" << std::endl;
-
-    // const uint64_t time_init_render = get_time();
     
     for (int j = 0; j < dims.value().height; j++) { 
         for (int i = 0; i < dims.value().width; i++) {
@@ -103,11 +187,8 @@ int main(int, char**) {
             pixels[index]     = b;
             pixels[index + 1] = g;
             pixels[index + 2] = r;
-            // pixels[index + 3] = 255;
         }
     }
-
-    // std::cout << "Pixels created" << std::endl;
 
     SDL_Surface* surface = SDL_CreateRGBSurfaceFrom((void*) pixels.data(),
             dims.value().width,
@@ -120,11 +201,8 @@ int main(int, char**) {
         return EXIT_FAILURE;
     }
 
-    // std::cout << "Surface created" << std::endl;
-
     SDL_Texture* texture = SDL_CreateTextureFromSurface(scr.renderer, surface);
 
-    // std::cout << "Texture created" << std::endl;
 
     SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_NONE);
     
@@ -140,22 +218,15 @@ int main(int, char**) {
     dstrect.w = scr.width();
     dstrect.h = scr.height();
 
-    // std::cout << "Rectangles created" << std::endl;
-
     SDL_RenderClear(scr.renderer);
 
     SDL_RenderCopy(scr.renderer, texture, &srcrect, &dstrect);
-    // std::cout << "Render copy" << std::endl;
 
-    SDL_RenderPresent(scr.renderer);
-
-    // const uint64_t time_render = get_time();
-    // std::cout << "Time to render: " << time_render - time_init_render
-    //     << " = " << 1000.0 / (time_render - time_init_render) << " fps" << std::endl;
-
-    // std::cout << "Render present" << std::endl;
+    // SDL_RenderPresent(scr.renderer);
 
     SDL_Texture* txt = SDL_CreateTexture(scr.renderer, SDL_PIXELFORMAT_BGR24, SDL_TEXTUREACCESS_STREAMING, scr.width(), scr.height());
+
+    mouse_pos mouse(scr.width(), scr.height());
 
     SDL_Event event;
     const uint64_t time_init = get_time();
@@ -163,13 +234,29 @@ int main(int, char**) {
     char* texture_pixels;
     int texture_pitch;
     const float ratio_x = ((float) (matrix.size() - 1)) / (scr.width()-1);
-    const float ratio_y = ((float) (matrix[0].size() - 1)) / (scr.height()-1);
+    // const float ratio_y = ((float) (matrix[0].size() - 1)) / (scr.height()-1);
     char* orig_pixels = (char*) surface->pixels;
+
+    const int img_width = dims.value().width;
+    // const int img_height = dims.value().height;
+
+    const int half_scr_width = scr.width() / 2;
+    const int half_scr_height = scr.height() / 2;
+
+    const float fov_x = 0.25; // 2pi * 0.25 = pi / 2
+    const float fov_y = 0.5;  // pi * 0.5 = pi / 2
+
+    screen_axes axes;
 
     while(true) {
         while(SDL_PollEvent(&event)) {
                     
-            if (event.type == SDL_QUIT ||
+            if (event.type == SDL_MOUSEMOTION) {
+                // std::cout << event.motion.xrel << ' ' << event.motion.yrel << std::endl;
+                mouse.set(event.motion.xrel, event.motion.yrel);
+                SDL_WarpMouseInWindow(scr.window, scr.width() >> 1, scr.height() >> 1);
+            }
+            else if (event.type == SDL_QUIT ||
                 (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)) {
                 const uint64_t curr_time = get_time();
                 std::cout << "Average fps: " << (1000.0 * frame_cpt) / (curr_time - time_init) << std::endl;
@@ -177,28 +264,26 @@ int main(int, char**) {
             }
         }
 
-        // Render new frame
+        // Render a new frame
+        const rt::vector center = get_center(fov_x, mouse.theta, mouse.phi);
+        axes.set(mouse.theta, mouse.phi);
+
         SDL_LockTexture(txt, NULL, (void**) &texture_pixels, &texture_pitch);
         for (int j = 0; j < scr.height(); j++) {
-            const int index_j = ratio_y * j;
-            for (int i = 0; i < scr.width(); i++) {
-                /*
-                rt::color& c = matrix[(int) (ratio_x * i)][index_j];
-                char r = c.get_red();
-                char g = c.get_green();
-                char b = c.get_blue();
-                const int index = 3 * (j * scr.width() + i);
-                texture_pixels[index]     = r;
-                texture_pixels[index + 1] = g;
-                texture_pixels[index + 2] = b;
-                */
 
-                // The spherical projection will be done with this index_source(i, j)
-                const int index_source = 3 * (index_j * dims.value().width + ((int) (ratio_x * i)));
+            const rt::vector y_component = axes.screen_y_axis * (half_scr_height - j) * (fov_y * PI / half_scr_height);
+
+            for (int i = 0; i < scr.width(); i++) {
+
+                const rt::vector x_component = axes.screen_x_axis * (i - half_scr_width) * (fov_x * PI / half_scr_width);
+                const rt::vector cartesian = center + x_component + y_component;
+                const spherical sph(cartesian);
+
+                const int index_src = index_source(sph.theta * (scr.width()-1) / 2*PI, sph.phi * (scr.height()-1) / 2*PI, ratio_x, img_width);
                 const int index = 3 * (j * scr.width() + i);
-                texture_pixels[index]     = orig_pixels[index_source];
-                texture_pixels[index + 1] = orig_pixels[index_source + 1];
-                texture_pixels[index + 2] = orig_pixels[index_source + 2];
+                texture_pixels[index]     = orig_pixels[index_src];
+                texture_pixels[index + 1] = orig_pixels[index_src + 1];
+                texture_pixels[index + 2] = orig_pixels[index_src + 2];
             }
         }
         SDL_UnlockTexture(txt);
@@ -209,54 +294,3 @@ int main(int, char**) {
     }
 
 }
-#if 0
-    mouse_pos mouse;
-    mouse.set(scr.width()/2, scr.height()/2);
-
-    const rt::color bg_col(0, 0, 0);
-    const rt::color cursor_col(255, 0, 0);
-    // update_screen_cursor(scr, bg_col, cursor_col, mouse);
-    update_screen(scr, matrix, mouse);
-
-    bool stop = false;
-
-    const uint64_t time_init = get_time();
-    uint64_t last_update_time = time_init;
-
-    unsigned int frame_cpt = 0;
-    
-    /* Update loop */
-
-    while (not stop) {
-        const uint64_t time = get_time();
-
-        SDL_Event event;
-        while(SDL_PollEvent(&event)) {
-                
-            if (event.type == SDL_MOUSEMOTION) {
-                mouse.set(event.motion.x, event.motion.y);
-            }
-            else if (event.type == SDL_QUIT ||
-                (event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)) {
-                stop = true;
-                break;
-            }
-        }
-
-        /* Update every 16ms, to target 60fps */
-        if (time - last_update_time >= 1000 / 60) {
-            // update_screen_cursor(scr, bg_col, cursor_col, mouse);
-            update_screen(scr, matrix, mouse);
-            scr.update();
-            last_update_time = time;
-            frame_cpt ++;
-        }
-    }
-
-    const uint64_t curr_time = get_time();
-
-    std::cout << "Average fps: " << (1000.0 * frame_cpt) / (curr_time - time_init) << std::endl;
-
-    return EXIT_SUCCESS;
-}
-#endif
