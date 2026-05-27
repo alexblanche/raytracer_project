092b1e9035b199b163c28197bc97eb31d61f1873
alexblanche <blanchealexandre11@yahoo.fr>
2026-05-27 13:58:33 +0200

    More file wrapper features

--- include/file_readers/file.hpp
+++ include/file_readers/file.hpp
@@ -33,6 +33,30 @@ class file {
             close();
         }
 
+        size_t length() const {
+            const size_t position = ftell(f);
+            fseek(f, 0, SEEK_END);
+            const size_t length = ftell(f);
+            fseek(f, position, SEEK_SET);
+            return length;
+        }
+
+        bool eof() const {
+            return feof(f);
+        }
+
+        size_t position() const {
+            return ftell(f);
+        }
+
+        void seek(size_t pos) const {
+            fseek(f, pos, SEEK_SET);
+        }
+
+        void rewind() const {
+            seek(0);
+        }
+
         template<class T>
         exit_status read(std::span<T> buffer) const {
             const int ret = fread(static_cast<void*>(buffer.data()), sizeof(T), buffer.size(), f);
@@ -46,25 +70,17 @@ class file {
             return exit_status_of(ret == extent);
         }
 
-        template<class T>
-        exit_status write(std::span<T> buffer) const {
-            const int ret = fwrite(static_cast<void*>(buffer.data()), sizeof(T), buffer.size(), f);
-            return exit_status_of(ret == buffer.size());
-        }
-
-        template<class T, size_t extent>
-        exit_status write(std::span<T, extent> buffer) const {
-            const int ret = fwrite(static_cast<void*>(buffer.data()), sizeof(T), extent, f);
-            return exit_status_of(ret == extent);
-        }
-
-        exit_status write(const std::string& s) const {
-            return exit_status_of(fprintf(f, "%s", s.c_str()));
+        int getc() const {
+            return fgetc(f);
         }
 
         exit_status skip(int n) const {
             std::vector<char> buffer(n);
             return read<char>({ buffer.data(), buffer.data() + n });
+        }
+
+        void skip_line() const {
+            while (!eof() && getc() != '\n');
         }
 
         template<typename... Args>
@@ -84,7 +100,8 @@ class file {
         requires std::is_integral_v<T>
         std::optional<T> scan() const {
             T x;
-            const exit_status status = scanf<T>("%llu", x);
+            constexpr std::string format = (std::is_signed_v<T>) ? "%lld" : "%llu";
+            const exit_status status = scanf<T>(format, x);
             return optional_of<T>(status, std::move(x));
         }
 
@@ -95,5 +112,51 @@ class file {
             constexpr std::string format = (sizeof(T) == sizeof(float)) ? "%f" : "%lf";
             const exit_status status = scanf<T>(format, x);
             return optional_of<T>(status, std::move(x));
+        }
+
+        std::string extract_from() const {
+            const size_t pos = position();
+            const size_t len = length() - pos;
+            std::string s(len + 1, '\0');
+            const exit_status status = read<char>(s);
+            printf("status = %s\n", status == exit_status::Success ? "success" : "failure");
+            return s;
+        }
+
+        std::string extract() const {
+            rewind();
+            return extract_from();
+        }
+
+        void cat() const {
+            const std::string content = extract();
+            std::printf("%s", content.c_str());
+        }
+
+        void cat_from() const {
+            const std::string content = extract_from();
+            std::printf("%s", content.c_str());
+        }
+
+        template<class T>
+        exit_status write(std::span<T> buffer) const {
+            const int ret = fwrite(static_cast<void*>(buffer.data()), sizeof(T), buffer.size(), f);
+            return exit_status_of(ret == buffer.size());
+        }
+
+        template<class T, size_t extent>
+        exit_status write(std::span<T, extent> buffer) const {
+            const int ret = fwrite(static_cast<void*>(buffer.data()), sizeof(T), extent, f);
+            return exit_status_of(ret == extent);
+        }
+
+        exit_status write(const std::string& s) const {
+            return exit_status_of(fprintf(f, "%s", s.c_str()));
+        }
+
+        template<typename... Args>
+        exit_status printf(const std::string& format, Args... args) const {
+            const int ret = fprintf(f, format.c_str(), args...);
+            return exit_status_of(ret != EOF);
         }
 };
--- src/tests/test_file.cpp
+++ src/tests/test_file.cpp
@@ -4,17 +4,17 @@ int main() {
 
     {
         file f("a.txt", "w");
-        f.write("12 -3 14  ");
+        f.write("12 -3 14  \n");
         f.close();
 
-        file f2("a.txt");
+        const file f2("a.txt");
         int a, b, c;
         f2.scanf("%d%d%d", a, b, c);
         printf("a = %d, b = %d, c = %d\n", a, b, c);
     }
 
     {
-        file f("a.txt", "r");
+        const file f("a.txt", "r");
         char t[5] = { 0 };
         f.read(std::span(t));
         f.skip(1);
@@ -28,7 +28,7 @@ int main() {
     }
 
     {
-        file f("a.txt", "r");
+        const file f("a.txt", "r");
         const uint32_t a = f.scan<uint32_t>().value();
         const  int32_t b = f.scan< int32_t>().value();
         const uint64_t c = f.scan<uint64_t>().value();
@@ -36,10 +36,26 @@ int main() {
     }
 
     {
-        file f("a.txt", "r");
+        const file f("a.txt", "r");
         const float  x = f.scan<float> ().value();
         const double y = f.scan<double>().value();
         printf("x = %f, y = %lf\n", x, y);
+    }
+
+    {
+        const file f("a.txt", "a");
+        const std::string s = "Alex";
+        const unsigned int n = 31;
+        f.printf("Je m'appelle %s, j'ai %u ans.\n", s.c_str(), n);
+    }
+
+    {
+        const file f("a.txt");
+        f.cat();
+        f.rewind();
+        printf("\n");
+        f.skip_line();
+        f.cat_from();
     }
 
     return EXIT_SUCCESS;
