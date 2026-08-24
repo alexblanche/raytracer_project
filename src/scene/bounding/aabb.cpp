74826643a1ad75a1e47bcb541456bf48933ae628
alexblanche <blanchealexandre11@yahoo.fr>
2026-08-24 14:52:52 +0200

    "Improved" aabb intersection testing. I cannot measure the speedup, as my perfs are too inconsistent

--- src/scene/bounding/aabb.cpp
+++ src/scene/bounding/aabb.cpp
@@ -7,39 +7,38 @@
 real aabb::measure_distance(const ray& r) const {
 
     const auto& [ u, dir, inv_dir ] = r;
-
-    const rt::vector v = u - position;
+    const auto& [ u_x, u_y, u_z ] = u - position;
 
     if constexpr (type_ == type::Corner) {
 
-        if ((v.x > 0.0_r && v.x < dims.x) && (v.y > 0.0_r && v.y < dims.y) && (v.z > 0.0_r && v.z < dims.z)) {
+        if ((u_x > 0.0_r && u_x < dims.x) && (u_y > 0.0_r && u_y < dims.y) && (u_z > 0.0_r && u_z < dims.z)) {
             // u is inside the box
             return 0.0_r;
         }
 
         /* Only one t can reach the box, so we return as soon as one fits */
 
-        const real tx = ((std::signbit(dir.x) ? dims.x : 0.0_r) - v.x) * inv_dir.x;
-        const real py = std::fma(dir.y, tx, v.y);
+        const real tx = ((std::signbit(dir.x) ? dims.x : 0.0_r) - u_x) * inv_dir.x;
+        const real py = std::fma(dir.y, tx, u_y);
         if (py > 0.0_r && py < dims.y) {
-            const real pz = std::fma(dir.z, tx, v.z);
+            const real pz = std::fma(dir.z, tx, u_z);
             if (pz > 0.0_r && pz < dims.z)
                 return tx > 0.0_r ? tx : infinity;
         }
         
-        const real ty = ((std::signbit(dir.y) ? dims.y : 0.0_r) - v.y) * inv_dir.y;
-        const real px = std::fma(dir.x, ty, v.x);
+        const real ty = ((std::signbit(dir.y) ? dims.y : 0.0_r) - u_y) * inv_dir.y;
+        const real px = std::fma(dir.x, ty, u_x);
         if (px > 0.0_r && px < dims.x) {
-            const real pz = std::fma(dir.z, ty, v.z);
+            const real pz = std::fma(dir.z, ty, u_z);
             if (pz > 0.0_r && pz < dims.z)
                 return ty > 0.0_r ? ty : infinity;
         }
 
-        const real tz = ((std::signbit(dir.z) ? dims.z : 0.0_r) - v.z) * inv_dir.z;
+        const real tz = ((std::signbit(dir.z) ? dims.z : 0.0_r) - u_z) * inv_dir.z;
         if (tz > 0.0_r) {
-            const real px = std::fma(dir.x, tz, v.x);
+            const real px = std::fma(dir.x, tz, u_x);
             if (px > 0.0_r && px < dims.x) {
-                const real py = std::fma(dir.y, tz, v.y);
+                const real py = std::fma(dir.y, tz, u_y);
                 return (py > 0.0_r && py < dims.y) ? tz : infinity;
             }
         }
@@ -50,19 +49,20 @@ real aabb::measure_distance(const ray& r) const {
     else if constexpr (type_ == type::Center) {
 
         // Check whether u is inside the box
-        if (std::abs(v.x) <= dims.x && std::abs(v.y) <= dims.y && std::abs(v.z) <= dims.z)
+        if (std::abs(u_x) <= dims.x && std::abs(u_y) <= dims.y && std::abs(u_z) <= dims.z)
             return 0.0_r;
 
-        const real t1 = (-v.x) * inv_dir.x - dims.x * std::abs(inv_dir.x);
-        if (std::abs(std::fma(dir.y, t1, v.y)) <= dims.y && std::abs(std::fma(dir.z, t1, v.z)) <= dims.z)
-            return t1 > 0.0_r ? t1 : infinity;
+        const real t_x = (-u_x - copysign(dims.x, dir.x)) * inv_dir.x;
+        if (std::abs(std::fma(dir.y, t_x, u_y)) <= dims.y && std::abs(std::fma(dir.z, t_x, u_z)) <= dims.z)
+            return t_x > 0.0_r ? t_x : infinity;
 
-        const real t2 = (-v.y) * inv_dir.y - dims.y * std::abs(inv_dir.y);
-        if (std::abs(std::fma(dir.x, t2, v.x)) <= dims.x && std::abs(std::fma(dir.z, t2, v.z)) <= dims.z)
-            return t2 > 0.0_r ? t2 : infinity;
+        const real t_y = (-u_y - copysign(dims.y, dir.y)) * inv_dir.y;
+        if (std::abs(std::fma(dir.x, t_y, u_x)) <= dims.x && std::abs(std::fma(dir.z, t_y, u_z)) <= dims.z)
+            return t_y > 0.0_r ? t_y : infinity;
 
-        const real t3 = (-v.z) * inv_dir.z - dims.z * std::abs(inv_dir.z);
-        return (t3 > 0.0_r && std::abs(std::fma(dir.x, t3, v.x)) <= dims.x && std::abs(std::fma(dir.y, t3, v.y)) <= dims.y) ?
-            t3 : infinity;
+        const real t_z = (-u_z - copysign(dims.z, dir.z)) * inv_dir.z;
+        return (t_z > 0.0_r
+            && std::abs(std::fma(dir.x, t_z, u_x)) <= dims.x && std::abs(std::fma(dir.y, t_z, u_y)) <= dims.y) ?
+            t_z : infinity;
     }
 }
